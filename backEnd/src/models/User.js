5d96f6d50298affdc8d858bb439ddae8f12b610a
Danrley Lima <danrleylac@gmail.com>
2023-11-30 17:51:21 -0300

    feat: add modelo de User

---
+++ backEnd/src/models/User.js
@@ -0,0 +1,33 @@
+import { Model, DataTypes } from "sequelize";
+
+import sequelize from "../config/database.js";
+// import Task from "./Task.js";
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "user",
+    timestamps: false,
+  }
+);
+
+export default User;
