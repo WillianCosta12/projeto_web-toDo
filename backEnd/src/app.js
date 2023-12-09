f20a83757e7813927c9d400244f69910b9b83842
Danrley Lima <danrleylac@gmail.com>
2023-12-09 13:58:54 -0300

    chore: instalando cors, bcrypt

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import express from "express";
 import sequelize from "./config/database.js";
 import taskRoutes from "./routes/TaskRoutes.js";
+import userRoutes from "./routes/UserRoutes.js";
 
 config();
 
@@ -9,13 +10,16 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-sequelize.sync().then(() => console.log("connected to the database"));
+sequelize
+  .sync({ force: true })
+  .then(() => console.log("connected to the database"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", taskRoutes);
+app.use("/api", userRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost${port}.`);
+  console.log(`Server is running on http://localhost:${port}.`);
 });
